d9b3e28409d09da3012c4badcbbd8995c91a6b33
philip putnam <putnamp989@gmail.com>
2017-01-20 09:44:07 -0800

    add userValid function and output to scripts.js ensuring user input is a valid number

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
+// Back-end, business logic
+
+var inputValid = function(number) {
+  if (number.match(/[^0-9]+/gi)) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+
+// Front-end, user interface logic
+
 $(document).ready(function() {
   $("#pong-form").submit(function(event) {
     event.preventDefault();
 
     var userInput = $("input#user-num").val();
 
-    $(".results").text(userInput);
+    if (!inputValid(userInput)) {
+      $(".results").text("Please type a rational non-negative number!");
+    } else {
+      $(".results").text(userInput);
+    }
 
   });
 
